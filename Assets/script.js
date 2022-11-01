99ed1eaa1da23d304d02871ec507ea88a44dc4b9
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-11-01 19:48:29 -0400

    added script.js api key

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// city
+var city = "";
+// city searched
+var citySearched = JSON.parse(localStorage.getItem("history")) || []; 
+// curent date & time
+var date = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+// Api Key
+var key = "7eb77816dcc99ae2a3625aa6fdd7fd5c" 
